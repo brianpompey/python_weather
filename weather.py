a6d4803f901dcdf22ac029eb8c5af7f58114b365
brianpompey <brianthescholar@gmail.com>
2021-09-09 21:47:33 -0400

    Accessing json data

--- weather.py
+++ weather.py
@@ -6,6 +6,8 @@ def getWeather():
     city = textfield.get()
     api = https://api.openweathermap.org/data/2.5/weather?q=" + city +"&appid=2a680c7a58c281fbfcd8d9a2a5dcb921
     json_data = requests.get(api).json()
+    condition = json_data['weather'][0]['main']
+    temp = json_data['main']['temp']
 
 
 canvas = tk.Tk()
